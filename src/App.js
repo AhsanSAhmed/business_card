05a38fbcf7b4a9d28d09263cf036c3e57b993cd1
AhsanSAhmed <ahsanahmedshajee@gmail.com>
2024-05-11 23:41:36 +0530

    Added styling to complete card

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import HeaderImage from "./components/HeaderImage"
-
+import Footer from "./components/Footer"
+import Main from "./components/Main";
 
 function App() {
   return (
     <div className="App">
       <HeaderImage />
+      <Main />
+      <Footer />
     </div>
   );
 }
