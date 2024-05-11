5703141aa393c6d66ec2874fbf8eb429157ccb1d
AhsanSAhmed <ahsanahmedshajee@gmail.com>
2024-05-11 23:41:00 +0530

    Created footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,18 @@
 import React from "react";
+import Github from "../Images/GitHub Icon.png"
+import twitter from "../Images/Twitter Icon.png"
+import facebook from "../Images/Facebook Icon.png"
+import instagram from "../Images/Instagram Icon.png"
 
 export default function Footer() {
   return (
     <div className="footerContainer">
-      
+        <nav className="footLogoBox">
+            <img src={twitter}></img>
+            <img src={facebook}></img>
+            <img src={instagram}></img>
+            <img src={Github}></img>
+        </nav>
     </div>
   );
 }
