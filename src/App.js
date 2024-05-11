18a52ddf27541867471a37da50e1faf7ed69e416
AhsanSAhmed <ahsanahmedshajee@gmail.com>
2024-05-11 21:55:47 +0530

    added an image to card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-import './App.css';
+import HeaderImage from "./components/HeaderImage"
+
 
 function App() {
   return (
     <div className="App">
-      
+      <HeaderImage />
     </div>
   );
 }
--- src/components/HeaderImage.js
+++ src/components/HeaderImage.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Office from "../Images/office.jpg"
 
 export default function HeaderImage() {
   return (
     <div className="headerContainer">
-      
+      <img src={Office} alt="PictureSelf"></img>
     </div>
   );
 }
